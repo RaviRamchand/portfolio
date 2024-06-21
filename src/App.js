d8a543861064b2f9dcfc258d94a1acc91207a0b4
Ravi Ramchand <45878442+RaviRamchand@users.noreply.github.com>
2024-06-21 13:56:13 -0400

    added basename to BrowswerRouter

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
