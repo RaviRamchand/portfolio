40bf145c5814e8282eb4b0a9e082546fbae9258d
Ravi Ramchand <45878442+RaviRamchand@users.noreply.github.com>
2024-06-30 11:04:19 -0400

    added favicon and modified hamburger colour

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,9 @@ import './style.css'
 function Header() {
     return (
         <div className="header-background">
-            <nav className="navbar navbar-expand-lg shadow p-3 mb-4 rounded">
+            <nav className="navbar navbar-expand-lg shadow p-3 mb-4 rounded navbar-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand source-code-pro header-colour" href="/">&lt;RaviRamchand/&gt;</a>
+                    <a className="navbar-brand source-code-pro header-colour" href="/">&lt;Ravi Ramchand/&gt;</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
